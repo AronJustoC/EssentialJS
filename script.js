34e9a8d74901001e09cd5ac097a733f5120af19c
AronJustoC <aronchoque727@gmail.com>
2024-12-31 18:06:16 -0500

    Add JavaScript examples like scope differences block vs function scope

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log("Todo lo necesario de JS");
  */
 
 //01. const-let-var
-//Ejercicio01 ------> verificamios const-let-var en un scope
+/////////////////////////Ejercicio01 ------> verificamios const-let-var en un scope
 /*
 const nameConst = "aron";
 let nameLet = "aron";
@@ -33,7 +33,7 @@ function verifyScope() {
 verifyScope();
 */
 
-//Ejercicio02 ------> verificamios const-let-var en un scope
+/////////////////////////Ejercicio02 ------> verificamios const-let-var en un scope
 /*
 function testScope() {
   if (true) {
@@ -48,7 +48,7 @@ function testScope() {
 testScope();
 */
 
-//Ejercicio03 ------> Reasignacion de variables
+/////////////////////////Ejercicio03 ------> Reasignacion de variables
 /*
 const a = 5;
 let b = 10;
@@ -61,7 +61,7 @@ c = 25; // ¿Qué pasa con la variable 'c'?
 console.log(b, c, a); //la varible a no se reasigna porque es una constante y nos dara un error
 */
 
-//Ejercicio04 ------> Hoisting de variables
+///////////////////////Ejercicio04 ------> Hoisting de variables
 /*
 console.log(a);//Imprime undefined
 console.log(b);//Se genera un error porque b no esta declarada
@@ -82,5 +82,99 @@ console.log(a);//Genera un error porque a no esta declarada esta es necesariamen
 console.log(b);
 console.log(c);
 */
+
+/////////////////////////Ejercicio06  ------> Escope Global vs Local
+/*
+var globalVar = "global";
+let globalLet = "global";
+const globalConst = "global";
+
+function checkGlobalScope() {
+  var localVar = "local";
+  let localLet = "local";
+  const localConst = "local";
+
+  console.log(globalVar); //se puede acceder a globalVar en el scope definido por la funcion checkGlobalScope
+  console.log(globalLet); //se puede acceder a globalLet en el scope definido por la funcion checkGlobalScope
+  console.log(globalConst); //se puede acceder a globalConst en el scope definido por la funcion checkGlobalScope
+  //console.log(localVar, localLet, localConst); // Fuera de funcion nos da error
+}
+
+checkGlobalScope();
+
+console.log(globalVar); //se puede acceder a globalVar en el scope global
+console.log(globalLet); //se puede acceder a globalLet en el scope global
+console.log(globalConst); //se puede acceder a globalConst en el scope global
+*/
+
+///////////////////////Ejercicio07 ------> Mutabilidad de objetos declarados con const
+/*
+const person = { name: "Aron", age: 28 };
+person.age = 29; // Sepuede modificar las propiedades de un objeto declarado con const
+console.log(person);
+//No se puede reasignar el objeto: person = {name: "Aron", age: 29}; Genera un error
+*/
+
+///////////////////////Ejercicio08 ------> Reasignacion dentro de bucles
+/*
+for (var i = 0; i < 10; i++) {
+  console.log(i); //i es accesible globalmente por usar var
+}
+
+console.log("Var fuera del bucle: ", i); // i es accesible globalmente por usar var te muestra el ultimo valor de i
+
+for (let j = 0; j < 10; j++) {
+  console.log(j); //j es accesible solo dentro del bloque por usar let
+}
+//console.log("Let fuera del bucle: ", j); // j no es accesible globalmente por usar let genera un error
+
+for (const k = 0; k < 10; k++) {
+  // console.log(k); nos da error porque no se puede reasignar una constante
+}
+*/
+
+///////////////////////Ejercicio09 ------> Variables dentro de funciones anidadas
+/*
+function outerFunction() {
+  const outerConst = "Outer";
+  let outerLet = "Outer";
+  var outerVar = "Outer";
+
+  function innerFunction() {
+    console.log(outerConst); //se puede acceder a outerConst en el scope definido por la funcion innerFunction
+    console.log(outerLet); //se puede acceder a outerLet en el scope definido por la funcion innerFunction
+    console.log(outerVar); //se puede acceder a outerVar en el scope definido por la funcion innerFunction
+  }
+  innerFunction();
+}
+outerFunction();
+*/
+
+/////////////////////////Ejercicio10 ------> Temporary Dead Zone (TDZ) con let y const
+/*
+{
+  //console.log(myVar); //undefined
+  //console.log(myLet); //Genera un error
+  //console.log(myConst); //Genera un error
+
+  var myVar = "Hoisted";
+  let myLet = "Not Hoisted";
+  const myConst = "Not Hoisted";
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
